287d003dac59d02931331145ca8261225a08e765
Brichnel <Randolphkengni@gmail.com>
2026-05-22 17:39:30 +0100

    correction docker

--- app/utils/key.py
+++ app/utils/key.py
@@ -1,4 +1,5 @@
 import math, random
+import string
 
 def generate_reference(key_length: int):
     key = ""
@@ -12,11 +13,16 @@ def generate_reference(key_length: int):
 
 
 def generate_key(key_length: int):
-    key = ""
+    min_length = max(key_length, 8)
+    required_chars = [
+        random.choice(string.ascii_uppercase),
+        random.choice(string.ascii_lowercase),
+        random.choice(string.digits),
+        random.choice("_@$%&/"),
+    ]
     key_element = "_0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_@$%&/"
+    remaining_chars = [random.choice(key_element) for _ in range(min_length - len(required_chars))]
+    key_chars = required_chars + remaining_chars
+    random.shuffle(key_chars)
 
-    length = len(key_element)
-    for i in range(key_length):
-        key += key_element[math.floor(random.random() * length)]
-
-    return key
+    return "".join(key_chars)
